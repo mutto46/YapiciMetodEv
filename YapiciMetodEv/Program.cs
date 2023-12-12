81aa392d5ed143a309c9b73741956c9950e0e11c
muttalip591 <muttalip591@BPC13>
2023-12-12 10:42:33 +0300

    Proje dosyası ekle.

---
+++ YapiciMetodEv/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YapiciMetodEv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Home home = new Home();
+            Console.WriteLine("Oda sayıs:{0} Metre Kare:{1}",home.OdaSayisi,home.MetreKare);
+        }
+       
+    }
+    class Home
+    {
+        int odaSayisi;
+        int metreKare;
+        public Home()  
+        {
+            odaSayisi = 12;
+            metreKare = 30;
+        }
+        public int OdaSayisi
+        {
+            get { return odaSayisi; }
+        }
+        public int MetreKare
+        {
+            get
+            {
+                return metreKare;
+            }
+        }
+    }
+
+}
